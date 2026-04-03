b908afeff16f043d4f48d6261947e59e7223edb7
swapnildani <sw_dani57@yahoo.com>
2026-04-03 21:46:06 +0530

    Updated Cypress test

--- cypress/e2e/Specs/mainpage.cy.js
+++ cypress/e2e/Specs/mainpage.cy.js
@@ -3,65 +3,77 @@ const pageobj = require('../Page-Objects/Pageobjects.page')
 const testdata = require('../../fixtures/testdata.json')
 
 describe('the-internet.herokuapp test ', () => {
-    /* beforeEach(() => {
+  /* beforeEach(() => {
 
+  cy.visit('/')
+
+})*/
+
+  it('TestCase1: Test home page base url', () => {
     cy.visit('/')
+  })
 
-  })*/
+  it('TestCase2: Test all page functionality', () => {
+    cy.visit('/')
+    cy.get(pageobj.ABtesting.abtesting).click()
+    cy.get(pageobj.ABtesting.heading).should('contain.text', 'A/B Test');
+    cy.get(pageobj.ABtesting.para).should('contain.text', testdata.ABtest.paratext);
+    eventFunc.AddDeletefunc()
+    cy.go('back');
+  })
 
-    it('TestCase1: Test home page base url', () => {
-        cy.visit('/')
+  it('TestCase3: Login using Basic Authentication Method1:', () => {
+    cy.visit('/basic_auth', {
+      auth: { username: 'admin', password: 'admin' }
     })
+    cy.contains('Congratulations').should('be.visible')
+  })
 
-    it('TestCase2: Test all page functionality', () => {
-        cy.visit('/')
-        cy.get(pageobj.ABtesting.abtesting).click()
-        cy.get(pageobj.ABtesting.heading).should('contain.text', 'A/B Test');
-        cy.get(pageobj.ABtesting.para).should('contain.text', testdata.ABtest.paratext);
-        eventFunc.AddDeletefunc()
-        cy.go('back');
+  it('TestCase4: Login using Basic Authentication Method2:', () => {
+    cy.visit('/basic_auth', {
+      auth: {
+        username: Cypress.env('username'),// for this we have added credentials in cypress.config.js file
+        password: Cypress.env('password'),
+      }
     })
+    cy.contains('Congratulations').should('be.visible')
 
-    it('TestCase3: Login using Basic Authentication Method1:', () => {
-        cy.visit('/basic_auth', {
-            auth: { username: 'admin', password: 'admin' }
-        })
-        cy.contains('Congratulations').should('be.visible')
-    })
+  })
 
-    it('TestCase4: Login using Basic Authentication Method2:', () => {
-            cy.visit('/basic_auth', {
-            auth: {
-            username: Cypress.env('username'),// for this we have added credentials in cypress.config.js file
-            password: Cypress.env('password'),
-             }
-        })
-            cy.contains('Congratulations').should('be.visible')
+  it('TestCase5 :Test count of broken images', () => {
+    cy.visit('/')
+    cy.get(':nth-child(4) > a').click()
+    eventFunc.BrokeImages()
+  })
 
-    })
+  it('TestCase6: Test checkboxes', () => {
+    cy.visit('/checkboxes')
+    cy.get('#checkboxes').should('be.visible')
+    cy.get('#checkboxes > :nth-child(1)').uncheck()
+    cy.get('#checkboxes > :nth-child(1)').should('not.be.checked')
+    cy.get('#checkboxes > :nth-child(3)').check()
+    cy.get('#checkboxes > :nth-child(3)').should('be.checked')
+    cy.get('#checkboxes > :nth-child(1)').check()
+    cy.get('#checkboxes > :nth-child(1)').should('be.checked')
+  })
 
-    it('TestCase5 :Test count of broken images', () => {
-        cy.visit('/')
-        cy.get(':nth-child(4) > a').click()
-        eventFunc.BrokeImages()
-    })
+  it('TestCase7: Context menu pop up message', () => {
+    cy.visit('/context_menu').wait(2000)
+    eventFunc.Contextpopup_Alert();
+  })
 
-    it('TestCase6: Test checkboxes',()=>{
-        cy.visit('/checkboxes')
-        cy.get('#checkboxes').should('be.visible')
-        cy.get('#checkboxes > :nth-child(1)').uncheck()
-        cy.get('#checkboxes > :nth-child(1)').should('not.be.checked')
-        cy.get('#checkboxes > :nth-child(3)').check()
-        cy.get('#checkboxes > :nth-child(3)').should('be.checked')
-        cy.get('#checkboxes > :nth-child(1)').check()
-        cy.get('#checkboxes > :nth-child(1)').should('be.checked')
-    })
+  it('TestCase8: Dropdownselection', () => {
+    cy.visit('/dropdown')
+    cy.get(pageobj.Dropdown.option).select('Option 1').should('be.visible').should('have.value', '1')
+    cy.get(pageobj.Dropdown.option).select('Option 2').should('be.visible').should('have.value', '2')
 
-    it.only('TestCase7: Context menu pop up message', () => {
-        cy.visit('/context_menu').wait(2000)
-        eventFunc.Contextpopup_Alert();  
-    })
+  })
 
+  it.only('Validate dynamic content structure', () => {
+    eventFunc.visit();
+    eventFunc.getContentRows().should('have.length', 3);
+    eventFunc.getImages().should('be.visible');
+  });
 
 })
 
